4503af80647056fc23de8060af93504d6805b43b
Hemu2125 <chhsk2125@gmail.com>
2025-10-27 00:27:48 +0530

    deployment

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "http://localhost:5000" || "https://chat-app-3-kb3s.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
